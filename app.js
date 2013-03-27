913e57c1973ac2b5366544b55b80636b39ad16ae
david++ <heaven.hell.or@gmail.com>
2013-03-27 22:52:17 +0800

    middleware: cookeParser and session

--- app.js
+++ app.js
@@ -9,16 +9,30 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var users = {
+  tj: { name: 'tj' }
+};
+
+
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser('shhhh, very secret'));
+  app.use(express.session());
   app.use(express.methodOverride());
+
+  app.use(function(req, res, next){
+    console.log('%s %s', req.method, req.url);
+    next();
+  });
+
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -27,8 +41,27 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+// middleware
+
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error
+    , msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
+});
+
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.post('/login', routes.login);
+app.get('/logout', routes.logout);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,43 @@
 
 exports.index = function(req, res){
   res.render('index', { title: 'DoIt !' });
+  console.log(req.session);
 };
+
+exports.login = function(req, res){
+	console.log(req.body);
+
+	res.redirect('/');
+
+	/*
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation 
+      req.session.regenerate(function(){
+        // Store the user's primary key 
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('login');
+    }
+  });
+*/
+}
+
+exports.logout = function(req, res){
+  // destroy the user's session to log them out
+  // will be re-created next request
+  req.session.destroy(function(){
+    res.redirect('/');
+    });
+}
