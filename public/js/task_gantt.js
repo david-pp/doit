02e1514ffc661468d92f2e9a58b0d29bf5eca057
david++ <heaven.hell.or@gmail.com>
2013-12-20 14:58:26 +0800

    Gantt显示

--- public/js/task_gantt.js
+++ public/js/task_gantt.js
@@ -4,6 +4,60 @@ function TaskGantt(status) {
 }
 
 TaskGantt.prototype.init = function(selector) {
+	// date parsing
+	gantt.config.xml_date="%Y-%m-%j";
+
+	// scale
+	gantt.config.date_scale="%d/%n";
+	gantt.config.scale_height = 90;
+
+	var weekScaleTemplate = function(date){
+		var dateToStr = gantt.date.date_to_str("%d/%n");
+		var endDate = gantt.date.add(gantt.date.add(date, 1, "week"), -1, "day");
+		return dateToStr(date) + " - " + dateToStr(endDate);
+	};
+
+	gantt.config.subscales = [
+		{unit:"week", step:1, template:weekScaleTemplate},
+		{unit:"day", step:1, date:"%D" }
+	];
+
+	// setting weekend & today
+	gantt.templates.scale_cell_class = function(date){
+		var today = new Date();
+		if(today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth() && today.getDate() == date.getDate()) {
+			return "today";
+		}
+
+        if(date.getDay()==0||date.getDay()==6){
+            return "weekend";
+        }
+    };
+
+    // columns
+    gantt.config.columns = [
+    	{name : 'id', label:"#", width:20, template: function(item) { return item.id; }},
+    	{name:"text", label:"Desc", tree:true, width:'*' }
+     ];
+
+    // tooltips
+    gantt.templates.tooltip_text = function(start,end,task){
+	    return "<b>单　号: </b>" + task.id + 
+	    "<br/><b>分　类: </b> "+task.category +
+	    "<br/><b>状　态: </b> "+task.status +
+	    "<br/><b>优先级: </b> "+task.priority +
+	    "<br/><b>策　划: </b> "+task.designer +
+	    "<br/><b>服务器: </b> "+task.server +
+	    "<br/><b>客户端: </b> "+task.client +
+	    "<br/><b>测　试: </b> "+task.qa;
+	};
+
+	// auto adjusting scale length
+	gantt.config.fit_tasks = true;
+
+	gantt.config.task_height = 25;
+    gantt.config.row_height = 30;
+
 	gantt.init(selector);
 
 	$.get("/ajax/tasklist?status=" + this.status, function(data, retstatus) {
