066c6c40eeb676b8de25d3022bd46f3ea71c559c
david++ <heaven.hell.or@gmail.com>
2013-12-26 19:00:16 +0800

    版本状态指示器

--- model/PlanVersion.js
+++ model/PlanVersion.js
@@ -10,6 +10,22 @@ function PlanVersion(properties) {
 	}
 }
 
+// 版本类型
+PlanVersion.category = {
+	invalid   : 0,	// 未知
+	single    : 1,  // 单区
+	multizone : 2,  // 大区
+	allzone   : 3,  // 全区
+};
+
+// 版本状态
+PlanVersion.status = {
+	created   	   : 0,  // 刚创建
+	debug_test     : 1,  // 内网测试中
+	release_test   : 2,  // 外网测试中
+	passed         : 3,  // 版本结束
+};
+
 PlanVersion.db = new DBHelper({
 		table: 'plan_version',
 		key:'id',
@@ -17,7 +33,7 @@ PlanVersion.db = new DBHelper({
 		fields: [
 			{ name:'id', type:'int(10)', default: 1},                          // 编号
 			{ name:'desc', type:'varchar(256)', default: ''},                  // 描述
-			{ name:'status', type:'int(10)', default: 0},                    // 状态
+			{ name:'status', type:'int(10)', default: 0},                      // 状态
 			{ name:'category', type:'int(10)', default: 0},                    // 分类
 			{ name:'time_create', type:'int(10)', default: 0},                 // 创建时间
 			{ name:'time_make', type:'int(10)', default: 0},                   // 版本时间           
--- public/js/task_plan.js
+++ public/js/task_plan.js
@@ -1,4 +1,45 @@
 
+function PlanVersion() {
+}
+
+// 版本类型
+PlanVersion.category = {
+	invalid   : 0,	// 未知
+	single    : 1,  // 单区
+	multizone : 2,  // 大区
+	allzone   : 3,  // 全区
+};
+
+// 版本状态
+PlanVersion.status = {
+	created   	   : 0,  // 刚创建
+	debug_test     : 1,  // 内网测试中
+	release_test   : 2,  // 外网测试中
+	passed         : 3,  // 版本结束
+};
+
+PlanVersion.category2text = function (category) {
+	switch (category) {
+		case 0: return "未知";
+		case 1: return "单区";
+		case 2: return "大区";
+		case 3: return "全区";
+	}
+
+	return "未知";
+}
+
+PlanVersion.status2img = function (status) {
+	switch (status) {
+		case 0: return '/codebase/imgs/verstatus_3_created.gif';
+		case 1: return '/codebase/imgs/verstatus_1_debugtest.gif';
+		case 2: return '/codebase/imgs/verstatus_2_releasetest.gif';
+		case 3: return '/codebase/imgs/verstatus_4_passed.gif';
+	}
+
+	return '/codebase/imgs/red.gif';
+}
+
 function TaskPlan () {
 	this.layout = null;
 	this.grid_plan = null;
@@ -9,11 +50,13 @@ function TaskPlan () {
 TaskPlan.prototype.init = function() {
 	this.layout = new dhtmlXLayoutObject(document.body, "3L");
 	this.layout.cells("a").setText("版本计划");
-	this.layout.cells("b").setText("单子");
+	this.layout.cells("b").setText("未发布的单子");
 	this.layout.cells("c").setText("版本内容");
 	this.layout.cells("a").setWidth(250);
 	//this.layout.cells("b").setHeight(250);
-	//this.layout.setCollapsedText("a", "版本计划");
+	this.layout.setCollapsedText("a", "版本计划");
+	this.layout.setCollapsedText("b", "未发布的单子");
+	this.layout.setCollapsedText("c", "版本内容");
 
 	var statusBar = this.layout.cells("a").attachStatusBar();
     statusBar.setText("Simple Status Bar");
@@ -26,10 +69,10 @@ TaskPlan.prototype.init = function() {
 TaskPlan.prototype.initPlan = function() {
 	this.grid_plan = this.layout.cells("a").attachGrid();	
 	this.grid_plan.setImagePath("/codebase/imgs/");
-	this.grid_plan.setHeader("日期,类型,描述");
-	this.grid_plan.setColAlign("center,center,left");
-	this.grid_plan.setColTypes("ro,ro,link");
-	this.grid_plan.setColSorting("date,str,str");
+	this.grid_plan.setHeader("状态,日期,类型,描述");
+	this.grid_plan.setColAlign("center,center,center,left");
+	this.grid_plan.setColTypes("img,ro,ro,link");
+	this.grid_plan.setColSorting("str,date,str,str");
 	this.grid_plan.setStyle("text-align:center;");
 	this.grid_plan.enableAutoWidth(true);
 	//this.grid_plan.enableAutoHeight(true);
@@ -52,10 +95,11 @@ TaskPlan.prototype.initPlan = function() {
 			data.rows[i] = new Object();
 			data.rows[i].data = new Array();
 
-			data.rows[i].id = serverdata[i].id;                    
-			data.rows[i].data[0] = Task.time2text(serverdata[i].time_make);
-			data.rows[i].data[1] = serverdata[i].category;
-			data.rows[i].data[2] = serverdata[i].desc + '^/p/' + serverdata[i].id;
+			data.rows[i].id = serverdata[i].id; 
+			data.rows[i].data[0] = PlanVersion.status2img(serverdata[i].status);                   
+			data.rows[i].data[1] = Task.time2text(serverdata[i].time_make);
+			data.rows[i].data[2] = PlanVersion.category2text(serverdata[i].category);
+			data.rows[i].data[3] = serverdata[i].desc + '^/p/' + serverdata[i].id;
 		}
 
 		_grid_plan.parse(data,"json");	
